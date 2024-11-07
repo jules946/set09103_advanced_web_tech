5b6f2ec4f6cdbfd5bb92d3a3ddf30f5c72698ea0
Julius Manowski <114394404+jules946@users.noreply.github.com>
2024-11-07 15:48:11 +0000

    Adding basic css sheet

--- src/app.py
+++ src/app.py
@@ -3,8 +3,9 @@ import os
 
 # setting templates folder path
 templates_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates')
+static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static')
 
-app = Flask(__name__, template_folder=templates_folder)
+app = Flask(__name__, template_folder=templates_folder, static_folder=static_folder)
 
 @app.route('/')
 def index():
