0b61a96bddce88c022cbace96b6a04aa0c3a8396
Julius Manowski <114394404+jules946@users.noreply.github.com>
2024-11-07 15:29:16 +0000

    Creating basic index and base template

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# setting templates folder path
+templates_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates')
 
+app = Flask(__name__, template_folder=templates_folder)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
