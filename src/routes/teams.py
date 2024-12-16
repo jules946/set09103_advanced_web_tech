4c3f2825dcf6ff7d2b30efe5dcdc80d25508caff
Julius Manowski <114394404+jules946@users.noreply.github.com>
2024-12-16 16:24:48 +0000

    Getting team logo pic from NBA API (kind of)

--- src/routes/teams.py
+++ src/routes/teams.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, abort
 from ..services.balldontlie_api import BDLAPIService
+from ..services.nba_api import NBAAPIService
 
 teams_bp = Blueprint('teams', __name__)
-nba_api = BDLAPIService()
+bdl_api = BDLAPIService()
+nba_api = NBAAPIService()
 
 
 @teams_bp.route('/team/<int:team_id>')
@@ -10,8 +12,17 @@ def team_details(team_id):
     """
     Get team details by ID
     """
-    team = nba_api.get_team(team_id)
+    team = bdl_api.get_team(team_id)
     if not team:
         abort(404)
 
-    return render_template('teams/details.html', team=team)
+    upcoming_team_games = bdl_api.get_team_games(team["id"], "upcoming")
+    recent_team_games = bdl_api.get_team_games(team["id"], "completed")
+    team_logo_url = nba_api.get_team_logo(team["full_name"])
+    return render_template(
+        'teams/details.html',
+        team=team,
+        upcoming_games=upcoming_team_games,
+        recent_games=recent_team_games,
+        team_logo_url=team_logo_url
+    )
