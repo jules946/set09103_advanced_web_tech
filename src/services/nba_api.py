e218a9235727aa4421f86768e423da023612dacf
Julius Manowski <114394404+jules946@users.noreply.github.com>
2024-12-16 14:07:40 +0000

    Using NBA API lib just for getting pics of players

--- src/services/nba_api.py
+++ src/services/nba_api.py
@@ -1,119 +1,30 @@
-import os
-import requests
-from typing import Dict, List
-from rapidfuzz import process, fuzz
+from nba_api.stats.static import players
 
-class NBAApiService:
+
+class NBAAPIService:
     """
-    Service class to interact with the balldontlie NBA API
+    Utility class for retrieving NBA player information and assets from nba_api library.
     """
     def __init__(self):
-        self.base_url = "https://api.balldontlie.io/v1"
-        self.headers = {"Authorization": os.environ.get('NBA_API_KEY')}
+        # luckily nba has done this for me :)
+        self.placeholder_pic_url = "https://cdn.nba.com/headshots/nba/latest/1040x760/placeholder.png"
 
     @staticmethod
-    def process_response(response):
-        """Process the response from the API"""
-        if response.status_code == 200:
-            try:
-                return response.json().get('data', [])
-            except ValueError as e:
-                print("JSONDecodeError:", e)
-        else:
-            print(f"Error: Received status code {response.status_code}")
-            print("Response Text:", response.text)
-        return []
-
-
-    def get_player(self, player_id):
-        """Get player details by ID"""
-        response = requests.get(f"{self.base_url}/players/{player_id}", headers=self.headers)
-        return self.process_response(response)
-
-    def search_players(self, name):
-        """Search players by name"""
-        params = {"search": name}
-        response = requests.get(f"{self.base_url}/players", headers=self.headers, params=params)
-        return self.process_response(response)
-
-    def search_teams(self, search_query):
+    def _get_player_id(first_name, second_name):
         """
-        Get lis of all teams.
+        Gets NBA player ID by first and last name.
         """
-        # fetch all teams from the API
-        response = requests.get(f"{self.base_url}/teams", headers=self.headers)
-        return self.process_response(response)
+        player = players.find_players_by_full_name(f"{first_name} {second_name}")
+        if player:
+            return player[0]['id']
+        return None
 
-    def get_team(self, team_id):
-        """Get team details by ID"""
-        response = requests.get(f"{self.base_url}/teams/{team_id}", headers=self.headers)
-        return self.process_response(response)
-
-    def unified_search(self, search_query: str) -> Dict[str, List]:
+    def get_player_picture_url(self, first_name, second_name):
         """
-        Perform a unified search for both players and teams,
-        returning results sorted by match relevance.
+        Get player picture URL by player ID.
         """
-        # get API results
-        players = self.search_players(search_query)
-        teams = self.search_teams(search_query)
-
-        # create unified results list with match scores
-        unified_results = []
-
-        # helper function to calculate normalized scores
-        def calculate_score(query: str, target: str) -> float:
-            # primary score using WRatio
-            calculated_score = fuzz.WRatio(query.lower(), target.lower())
-
-            # adjust score if search query is a substring of target
-            # e.g. "LeBron" should match "LeBron James"
-            if query.lower() in target.lower():
-                calculated_score += int((len(query) / len(target)) * 20)
-
-            return min(calculated_score, 100)
-
-        # process players
-        for player in players:
-            name = f"{player['first_name']} {player['last_name']}"
-            score = calculate_score(search_query, name)
-
-            if score > 80:
-                unified_results.append({
-                    "type": "player",
-                    "data": player,
-                    "score": score
-                })
-
-        # process teams
-        for team in teams:
-            # calculate match scores for both full_name and name
-            full_name_score = calculate_score(search_query, team["full_name"])
-            name_score = calculate_score(search_query, team["name"])
-
-            # only consider the higher of the two scores
-            score = max(full_name_score, name_score)
-
-            if score > 80:
-                unified_results.append({
-                    "type": "team",
-                    "data": team,
-                    "score": score
-                })
-
-        # sort all results by score, descending
-        unified_results.sort(key=lambda x: x['score'], reverse=True)
-
-        return {
-            "results": unified_results,
-            "total_results": len(unified_results)
-        }
-
-    def get_player_stats(self, player_id, season=2024):
-        """Get player stats for a specific season"""
-        params = {
-            "player_id": player_id,
-            "season": season
-        }
-        response = requests.get(f"{self.base_url}/season_averages", headers=self.headers, params=params)
-        return self.process_response(response)
+        # This isn't great but... it works, until nba will change url structure
+        player_id = self._get_player_id(first_name, second_name)
+        if player_id:
+            return f"https://cdn.nba.com/headshots/nba/latest/1040x760/{player_id}.png"
+        return self.placeholder_pic_url
