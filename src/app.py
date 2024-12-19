68c95021865e7021e1b6e327ebbd23763edf1f00
Julius Manowski <114394404+jules946@users.noreply.github.com>
2024-12-19 23:10:29 +0000

    Bug Fixing Heroku not displaying 404

--- src/app.py
+++ src/app.py
@@ -7,21 +7,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=False)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template(
-        'error.html',
-        code=404,
-        message="Page Not Found",
-        description="The page you're looking for doesn't exist or has been moved."
-    ), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template(
-        'error.html',
-        code=500,
-        message="Internal Server Error",
-        description="Something went wrong on our end. Please try again later."
-    ), 500
